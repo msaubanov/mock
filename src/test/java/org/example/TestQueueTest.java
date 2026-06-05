4035b666135e9963bc56c177c55f8028dc05bb26
Yoga <maratsaubanov7@gmail.com>
2026-06-06 00:49:20 +0500

    TestStack and queue

--- src/test/java/org/example/TestQueueTest.java
+++ src/test/java/org/example/TestQueueTest.java
@@ -124,6 +124,7 @@ class TestQueueTest {
         Assertions.assertEquals(Thread.State.WAITING,thread.getState());
         queue.put(1);
         latch.await();
+        thread.join();
         Assertions.assertEquals(Thread.State.TERMINATED,thread.getState());
     }
 
